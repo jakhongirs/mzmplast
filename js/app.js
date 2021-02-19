89af84891fe57abf9a5012ed91f862530a85e526
jakhongirs <j.sagdullaev31@gmail.com>
2021-02-19 19:51:48 +0500

    Completed delivery and contacts sections

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 var elButtonToggler = document.querySelector('.sitenav__toggle');
-    var elButton = document.querySelector('.sitenav');
+var elButton = document.querySelector('.sitenav');
+var elMapDiv = document.querySelector('.contacts__map-wrapper');
 
-    elButtonToggler.addEventListener('click', function () {
-      elButton.classList.toggle('sitenav--open');
-    });
+elButtonToggler.addEventListener('click', function () {
+  elButton.classList.toggle('sitenav--open');
+});
+
+elMapDiv.addEventListener("click", function() {
+  var elMap = document.querySelector('.contacts__map');
+
+  elMap.classList.add("contacts__map--clicked");
+});
